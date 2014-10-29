d207c2d3c54e7b737889f8e25f690c7cca001ab3
Jeff Liu <mangomystery@gmail.com>
2014-10-28 19:51:17 -0700

    add authentication and register/sign-in paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
+  root to: 'pages#front'
   get '/home', to: 'videos#index'
+
   resources :videos, only: [:show] do |variable|
     collection do
-      get :search, to: "videos#search"
+      get :search, to: 'videos#search'
     end
   end
   resources :categories, only: [:show]
+
+  get '/register', to: 'users#new'
+  resources :users, only: [:create]
+
+  get '/sign_in', to: 'sessions#new'
+  resources :sessions, only: [:create]
+  get '/sign_out', to: 'sessions#destroy'
 end
