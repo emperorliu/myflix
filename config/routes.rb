b1a63efdbb1ca9e29f07b755e9a498513e2485a0
Jeff Liu <mangomystery@gmail.com>
2014-11-05 16:46:18 -0800

    reordering videos in the queue

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,6 @@ Myflix::Application.routes.draw do
   get '/sign_out', to: 'sessions#destroy'
 
   resources :queue_items, only: [:create, :destroy]
-  get 'my_queue', to: 'queue_items#index'
+  get '/my_queue', to: 'queue_items#index'
+  post 'update_queue', to: 'queue_items#update_queue'
 end
