1dea0f3083975563a4367858b7eb3f81e71755ef
Jeff Liu <mangomystery@gmail.com>
2014-11-06 12:23:10 -0800

    my queue ratings

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -8,9 +8,22 @@ class QueueItem < ActiveRecord::Base
   validates_numericality_of :position, {only_integer: true}
 
   def rating
-    review = Review.where(user_id: user.id, video_id: video.id).first
     review.rating if review
   end
 
+  def rating=(new_rating) #write setter method
+    if review
+      review.update_column(:rating, new_rating) #update_column bypass validations, update_attributes requires rating to not be nil, special syntax :rating for update_column
+    else
+      review = Review.new(user: user, rating: new_rating)
+      review.save(validate: false) #bypass content validation so review can have nil content when saving review for last test
+    end
+  end
+
+  private
+
+  def review
+    @review ||= Review.where(user_id: user.id, video_id: video.id).first
+  end
   #video.categories is now delegated
 end
--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -29,6 +29,35 @@ describe QueueItem do
     end
   end
 
+  describe "#rating=" do
+    it "changes the rating of the review if the review is present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, user: user, video: video, rating: 2)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = 4
+      expect(Review.first.rating).to eq(4)
+    end
+
+    it "clears the rating of the review if the review is present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, user: user, video: video, rating: 2)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = nil
+      expect(Review.first.rating).to be_nil
+    end
+
+    it "will create a review with the rating if the review is not present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = 3
+      expect(Review.first.rating).to eq(3)
+      # have to bypass nil-content validation
+    end
+  end
+
 
   # describe "#category" do
   #   it "returns the category of the video" do
