f6bea3d9272bfc299c339779343634472f145713
Jeff Liu <mangomystery@gmail.com>
2014-11-06 12:38:04 -0800

    queue rating stuff

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -15,7 +15,7 @@ class QueueItem < ActiveRecord::Base
     if review
       review.update_column(:rating, new_rating) #update_column bypass validations, update_attributes requires rating to not be nil, special syntax :rating for update_column
     else
-      review = Review.new(user: user, rating: new_rating)
+      review = Review.new(user: user, video: video, rating: new_rating)
       review.save(validate: false) #bypass content validation so review can have nil content when saving review for last test
     end
   end
