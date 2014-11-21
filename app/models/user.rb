0bf6e6abf2a9328c9e7ec6009b2381db9a975632
Jeff Liu <mangomystery@gmail.com>
2014-11-20 16:22:24 -0800

    welcome email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :reviews, ->{ order("created_at DESC")}
   has_many :queue_items, ->{ order(position: :asc) }
   has_many :following_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :leader_relationships, class_name: "Relationship", foreign_key: :leader_id
 
   has_secure_password validations: false
 
@@ -25,5 +26,6 @@ class User < ActiveRecord::Base
   def can_follow?(another_user)
     !(another_user == self || self.follows?(another_user))
   end
+
 
 end
