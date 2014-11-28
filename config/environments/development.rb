ba6c05c480a4fc5285d4ede02078af1334cf626d
Jeff Liu <mangomystery@gmail.com>
2014-11-27 16:34:46 -0800

    mailgun changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,11 +23,11 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:              ENV['MAILGUN_SMTP_SERVER'],
-  port:                 ENV['MAILGUN_SMTP_PORT'],
-  domain:               'secret-island-5222.herokuapp.com',
-  user_name:            ENV['MAILGUN_SMTP_LOGIN'],
-  password:             ENV['MAILGUN_SMTP_PASSWORD'],
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'secret-island-5222.herokuapp.com',
+    :authentication => :plain,
+  }
 end
