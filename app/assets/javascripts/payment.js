11b08b98cb23edb940bbf078330643dbc8ae6849
Jeff Liu <mangomystery@gmail.com>
2014-12-09 16:51:05 -0800

    custom payment stripe form

---
+++ app/assets/javascripts/payment.js
@@ -0,0 +1,22 @@
+jQuery(function($) {
+  $('#payment-form').submit(function(event) {
+    var $form = $(this);
+    $form.find('button').prop('disabled', true);
+    Stripe.card.createToken($form, stripeResponseHandler);
+    return false;
+  });
+});
+
+function stripeResponseHandler(status, response) {
+  var $form = $('#payment-form');
+
+  if (response.error) {
+    $form.find('.payment-errors').text(response.error.message);
+    $form.find('button').prop('disabled', false);
+  } else {
+    // response contains id and card, which contains additional card details
+    var token = response.id;
+    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+    $form.get(0).submit();
+  }
+};
